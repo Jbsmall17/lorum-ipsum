3aabc4ae5ebcd98a83eec93c90bbd2718132edaa
Jbsmall17 <alaomiyaki17@gmail.com>
2023-01-21 07:21:45 -0800

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,40 @@ import React, { useState } from 'react'
 import data from "./data"
 
 function App() {
-
+  const [count,setCount] = useState(0)
+  const [paragraphs,setParagraphs] = useState([])
+  const handleSubmit = (e) =>{
+    e.preventDefault()
+    let amount = parseInt(count)
+    if(amount < 0){
+      amount = 1 
+    }
+    else if(amount > data.length){
+      amount = data.length
+    }
+    setParagraphs(data.slice(0,amount))
+  }
   return (
-    <div className="App">
-      LOREM IPSUM SETUP
-    </div>
+    <section className="section-center">
+      <h3>TIRED OF BORING LOREM IPSUM? </h3>
+      <form onSubmit={handleSubmit} className="lorem-form">
+        <label htmlFor='amount'>Paragraphs:</label>
+        <input 
+          type="number" 
+          id="amount"
+          value = {count}
+          onChange={(e)=>{setCount(e.target.value)}}
+          ></input>
+        <button type='submit' className='btn'>generate</button>
+      </form>
+      <article className="lorem-text">
+          {
+            paragraphs.map((paragraph,index)=>{
+              return <p key={index}>{paragraph}</p>
+            })
+          }
+        </article>
+    </section>
   )
 }
 
