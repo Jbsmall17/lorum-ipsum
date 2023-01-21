84bfa5b89fbcaa96ab8110914c2fa5ac0efadc2a
Jbsmall17 <alaomiyaki17@gmail.com>
2023-01-21 06:44:01 -0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+import data from "./data"
+
+function App() {
+
+  return (
+    <div className="App">
+      LOREM IPSUM SETUP
+    </div>
+  )
+}
+
+export default App
